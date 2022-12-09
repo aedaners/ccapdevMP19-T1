e24c0730745e9f35cde8f6706bcd7e7e85d9008b
aedaners <87559949+aedaners@users.noreply.github.com>
2022-12-09 14:18:32 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const port = 3000
+
+//static file
+app.use(express.static("public"))
+app.use('/css',express.static(__dirname + 'public/css'))
+app.use('/js',express.static(__dirname + 'public/js'))
+app.use('/images',express.static(__dirname + 'public/images'))
+
+//set views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/login', function(req, res) {
+    res.sendFile(__dirname + "/login.ejs");
+});
+
+/*
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/views/index.html")
+});
+
+app.get('/login', function(req, res) {
+    res.sendFile(__dirname + "/views/login.html")
+});
+*/
+
+//app.listen(port, () => console.info(`Listening on port ${port}`))
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server started on port 3000");
+  });
+
+//Connect to MongoDB
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://dbCCAPDEV19:dbCCAPDEV19@ccapdevmp.pojqxdn.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("ccapdevmp").collection("mpdata");
+  console.log("Connected to MongoDB");
+  client.close();
+});
